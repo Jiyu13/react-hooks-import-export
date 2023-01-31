16c3a8c1913a1c187fc63e7cd4af33581f80b6d3
Ziru Zou <jiyucoding@yahoo.com>
2023-01-31 16:53:51 -0500

    import & export

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,26 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// import {trees, wildlife, elevation} from "./parks/RockyMountain"
+import * as RMFuctions from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  console.log(RMFuctions.trees)
+  RMFuctions.wildlife()
+  RMFuctions.elevation()
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <div>
+        <MesaVerde/>
+      </div>
+      <div>
+        <div>{RMFuctions.trees}</div>      
+      </div>
+    </div>
+  )
 }
+
+export default ColoradoStateParks
